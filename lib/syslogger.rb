d4aa9ef01b4ef7c75503b1e70220164dea1afaef
Cyril <cyril.rohr@gmail.com>
2010-03-05 10:57:17 +0100

    correctly implemented #level method

--- lib/syslogger.rb
+++ lib/syslogger.rb
@@ -2,12 +2,14 @@ require 'syslog'
 require 'logger'
 
 class Syslogger
+  attr_reader :level, :ident, :options, :facility
+
   MAPPING = {
     Logger::DEBUG => Syslog::LOG_DEBUG,
     Logger::INFO => Syslog::LOG_INFO,
-    Logger::WARN => Syslog::LOG_WARNING,
-    Logger::ERROR => Syslog::LOG_ERR,
-    Logger::FATAL => Syslog::LOG_EMERG,
+    Logger::WARN => Syslog::LOG_NOTICE,
+    Logger::ERROR => Syslog::LOG_WARNING,
+    Logger::FATAL => Syslog::LOG_ERR,
     Logger::UNKNOWN => Syslog::LOG_ALERT
   }
   # 
@@ -15,24 +17,32 @@ class Syslogger
   # <tt>ident</tt>:: the name of your program [default=$0]
   # <tt>options</tt>:: Syslog options [default=Syslog::LOG_PID | Syslog::LOG_CONS]
   # <tt>facility</tt>:: the syslog facility [default=nil]
+  #
   #                     correct values are Syslog::LOG_DAEMON, Syslog::LOG_USER, Syslog::LOG_SYSLOG, Syslog::LOG_LOCAL2, Syslog::LOG_NEWS, etc.
   # 
   # Usage:
-  #   logger = Syslog
+  #   logger = Syslogger.new("my_app", Syslog::LOG_PID | Syslog::LOG_CONS, Syslog::LOG_LOCAL0)
+  #   logger.level = Logger::INFO # use Logger levels
+  #   logger.warn "warning message"
+  #   logger.debug "debug message"
+  # 
   def initialize(ident = $0, options = Syslog::LOG_PID | Syslog::LOG_CONS, facility = nil)
     @ident = ident
     @options = options || (Syslog::LOG_PID | Syslog::LOG_CONS)
     @facility = facility
+    @level = Logger::INFO
   end
   
   %w{debug info warn error fatal unknown}.each do |logger_method|
-  # {:debug => :debug, :info => :info, :warn => :warning, :error => :err, :fatal => :emerg, :unknown => :alert}.each do |logger_method, syslog_method|
     define_method logger_method.to_sym do |message|
-      Syslog.open(@ident, @options, @facility) { |s| s.log(MAPPING[Logger.const_get(logger_method.upcase)], message) }
+      Syslog.open(@ident, @options, @facility) { |s| 
+        s.mask = Syslog::LOG_UPTO(MAPPING[@level])
+        s.log(MAPPING[Logger.const_get(logger_method.upcase)], message) 
+      }
     end
   end
   
   def level=(logger_level)
-    Syslog.mask = Syslog::LOG_UPTO(MAPPING.invert[logger_level])
+    @level = logger_level
   end
 end
--- spec/syslogger_spec.rb
+++ spec/syslogger_spec.rb
@@ -3,16 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Syslogger" do
   it "should log to the default syslog facility, with the default options" do
     logger = Syslogger.new
-    logger.should respond_to(:error)
-    Syslog.should_receive(:open).with($0, Syslog::LOG_PID | Syslog::LOG_CONS, nil).and_yield(syslog=mock("syslog"))
-    syslog.should_receive(:log).with(Syslog::LOG_WARNING, "Some message")
+    Syslog.should_receive(:open).with($0, Syslog::LOG_PID | Syslog::LOG_CONS, nil).and_yield(syslog=mock("syslog", :mask= => true))
+    syslog.should_receive(:log).with(Syslog::LOG_NOTICE, "Some message")
     logger.warn "Some message"
   end
   
   it "should log to the user facility, with specific options" do
     logger = Syslogger.new("my_app", Syslog::LOG_PID, Syslog::LOG_USER)
-    Syslog.should_receive(:open).with("my_app", Syslog::LOG_PID, Syslog::LOG_USER).and_yield(syslog=mock("syslog"))
-    syslog.should_receive(:log).with(Syslog::LOG_WARNING, "Some message")
+    Syslog.should_receive(:open).with("my_app", Syslog::LOG_PID, Syslog::LOG_USER).and_yield(syslog=mock("syslog", :mask= => true))
+    syslog.should_receive(:log).with(Syslog::LOG_NOTICE, "Some message")
     logger.warn "Some message"
   end
   
@@ -21,4 +20,6 @@ describe "Syslogger" do
       Syslogger.new.should respond_to logger_method.to_sym
     end
   end
+  
+  # TODO: test logger level
 end
